f04e20e3e5ee0e9c743e0172cc787790527db0a4
Justin Hedani <jkhedani@gmail.com>
2018-04-30 21:17:59 -0700

    Initial React configuration

---
+++ README.md
@@ -0,0 +1,12 @@
+# Parcel Bootstrap
+An attempt at trying to replicate the primary features of `create-react-app`
+using Parcel.
+
+
+TODO:
+	- Add React
+	- Add ES6 Compiler
+	- Add hot reloading
+	- Add SCSS parser
+  - Comparison
+  	- Review index.html
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// TODO: Import CSS
+
+const App = () => (
+	<div className="App">
+		<h1>I love you, Justin!</h1>
+	</div>
+)
+
+ReactDOM.render(
+	<App />
+	document.getElementById("root")
+)
