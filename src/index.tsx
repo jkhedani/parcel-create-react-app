64393b8cdfedcb699956b03f3ae70d782c244f32
Justin Hedani <jkhedani@gmail.com>
2018-11-16 11:53:01 -0800

    Add login as a secondary route

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'mobx-react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
-import { Home, NoMatch } from './routes';
+import { Home, Login, NoMatch } from './routes';
 import "./styles/index.scss"
 import * as stores from './stores';
 
@@ -12,6 +12,7 @@ const App = () => (
 		<Router>
 			<div id="wrapper">
         <Switch>
+          <Route exact path="/login" component={Login} />
           <Route exact path="/" component={Home} />
           <Route component={NoMatch} />	    		
         </Switch>
